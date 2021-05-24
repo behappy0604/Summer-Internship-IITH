713fa937e1a4d9cd23ed15fbdce0eef270d479c5
Ojaswa Pandey <69142765+behappy0604@users.noreply.github.com>
2021-05-24 10:52:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import math
 
 AB, A, B = 5, (85/180)*math.pi, (115/180)*math.pi
 
+
 def drawTriangle(AB, A, B):
     # drawing AB line of 5cm
-    plt.plot((0,5), (0,0))
+    plt.plot((0, 5), (0, 0))
 
-    lineLength = 10;
+    lineLength = 10
 
-    (x1, x2) = (0,lineLength * math.cos(A))
-    (y1, y2) = (0,lineLength * math.sin(A))
+    (x1, x2) = (0, lineLength * math.cos(A))
+    (y1, y2) = (0, lineLength * math.sin(A))
 
     (x3, x4) = (5, 5-lineLength * math.cos(B))
-    (y3, y4) = (0 , lineLength * math.sin(B)) 
+    (y3, y4) = (0, lineLength * math.sin(B))
 
     plt.plot((x1, x2), (y1, y2))
     plt.plot((x3, x4), (y3, y4))
@@ -26,14 +27,22 @@ def drawTriangle(AB, A, B):
 
 drawTriangle(AB, A, B)
 
+
 def isIntersecting(x1, x2, y1, y2, x3, x4, y3, y4):
     den = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
-    if(den == 0): return False 
-    t = ((xs1[0] - xs2[0]) * (ys1[0] - y4) - (ys1[0] - ys1[0]) * (xs2[0] - x4)) / den
-    u = -((xs1[0] - xs1[1]) * (ys1[0] - ys1[0]) - (ys1[0] - y2) * (xs1[0] - xs2[0])) / den
-    if (not (t > 0 and t < 1 and u > 0)): return False
+    if(den == 0):
+        return False
+    t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den
+    u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den
+    if (not (t > 0 and t < 1 and u > 0)):
+        return False
     return True
 
 
-isIntersecting(x1, x2, y1, y2, x3, x4, y3, y4)
+output = isIntersecting(x1, x2, y1, y2, x3, x4, y3, y4)
+
+if(output):
+    print("Both lines are not intersecting")
+else:
+    print("Both lines are not intersecting")
 
