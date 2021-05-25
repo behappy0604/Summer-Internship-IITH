e1cc9aea8964fa8e775f8a93e3ed369cdae91220
Ojaswa Pandey <69142765+behappy0604@users.noreply.github.com>
2021-05-25 01:06:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import math
 
 AB, A, B = 5, (85/180)*math.pi, (115/180)*math.pi
 
+lineLength = 10
+
+(x1, x2) = (0, lineLength * math.cos(A))
+(y1, y2) = (0, lineLength * math.sin(A))
+
+(x3, x4) = (5, 5-lineLength * math.cos(B))
+(y3, y4) = (0, lineLength * math.sin(B))
+
 
 def drawTriangle(AB, A, B):
     # drawing AB line of 5cm
     plt.plot((0, 5), (0, 0))
-
-    lineLength = 10
-
-    (x1, x2) = (0, lineLength * math.cos(A))
-    (y1, y2) = (0, lineLength * math.sin(A))
-
-    (x3, x4) = (5, 5-lineLength * math.cos(B))
-    (y3, y4) = (0, lineLength * math.sin(B))
 
     plt.plot((x1, x2), (y1, y2))
     plt.plot((x3, x4), (y3, y4))
@@ -29,7 +29,7 @@ drawTriangle(AB, A, B)
 
 
 def isIntersecting(x1, x2, y1, y2, x3, x4, y3, y4):
-    den = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
+    den = (x1-x2)(y3-y4) - (y1-y2)(x3-x4)
     if(den == 0):
         return False
     t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den
@@ -42,7 +42,7 @@ def isIntersecting(x1, x2, y1, y2, x3, x4, y3, y4):
 output = isIntersecting(x1, x2, y1, y2, x3, x4, y3, y4)
 
 if(output):
-    print("Both lines are intersecting then it  is a triangle")
+    print("Both lines are intersecting")
 else:
     print("Both lines are not intersecting")
     
